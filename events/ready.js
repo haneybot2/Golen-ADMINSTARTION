148edf2aa0dada53c1833ab3e63acb0cc91c4b17
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-02-08 16:22:32 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,4 @@
 module.exports = (client) => {
-	client.getScore = client.sql.prepare("SELECT * FROM scores WHERE user = ? AND guild = ?");
-	client.setScore = client.sql.prepare("INSERT OR REPLACE INTO scores (id, user, guild, points, level) VALUES (@id, @user, @guild, @points, @level);");
 	client.user.setActivity("Loading..", { type: "PLAYING" });
 	client.user.setStatus("dnd");
 	setTimeout(() => {
