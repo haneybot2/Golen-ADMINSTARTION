7a8c51d0140df5e3a442f9ff6fcebd8b3d1ef0b7
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-02-08 16:23:46 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -6,7 +6,7 @@ module.exports = (client) => {
 		client.user.setStatus("idle");
 	}, 1500);
 	setTimeout(() => {
-		client.user.setActivity("Colden", { type: "STREAMING", url: "https://www.twitch.tv/idk" });
+		client.user.setActivity("Golden", { type: "STREAMING", url: "https://www.twitch.tv/idk" });
 		client.user.setStatus("online");
 		console.log("[Start] " + new Date());
 	}, 2500);
