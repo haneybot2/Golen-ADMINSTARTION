308c2481ea9cfc054e4f717d9c5deb738d71e60c
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-02-08 16:42:51 +0200

    Delete voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++
@@ -1,8 +0,0 @@
-module.exports = (client, oldMember, newMember) => {
-      const channel = oldMember.guild.channels.get(client.config.voiceOnline)
-      || newMember.guild.channels.get(client.config.voiceOnline);
-      const currentSize = oldMember.guild.members.filter(m => m.voiceChannel).size
-      || newMember.guild.members.filter(m => m.voiceChannel).size;
-      const size = channel.name.match(/\[\s(\d+)\s\]/);
-      if (!size || currentSize !== size) channel.setName(`Golden Online: [${currentSize}]`);
-}
