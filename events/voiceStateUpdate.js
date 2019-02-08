6e89477d391efa977006f6d8e7b9e233a8f6bc39
haneybot2 <41707988+haneybot2@users.noreply.github.com>
2019-02-08 16:25:19 +0200

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,16 +1,6 @@
 module.exports = (client, oldMember, newMember) => {
-      // const channel = oldMember.guild.channels.get(client.config.voiceOnline);
-      // const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
-      // const size = channel.name.match(/\[\s(\d+)\s\]/);
-      // if (!size || currentSize !== size) channel.setName(`Miracle Online: [${currentSize}]`);
-      if (
-            (oldMember.id === client.user.id)
-            && (oldMember.voiceChannelID !== newMember.voiceChannelID)
-            && !newMember.voiceChannel
-      ) {
-            const queue = client.queue.get(oldMember.guild.id);
-            if (queue) {
-                  client.queue.delete(oldMember.guild.id)
-            }
-      }
+      const channel = oldMember.guild.channels.get(client.config.voiceOnline);
+      const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
+      const size = channel.name.match(/\[\s(\d+)\s\]/);
+      if (!size || currentSize !== size) channel.setName(`Golden Online: [${currentSize}]`);
 }
